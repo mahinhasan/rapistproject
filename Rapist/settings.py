8cd59d40b15145e3891a3d90afb384c64cd0ffcb
mahinhasan <36971968+mahinhasan@users.noreply.github.com>
2020-10-12 20:52:47 +0600

    Update settings.py

--- Rapist/settings.py
+++ Rapist/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFIELS_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
